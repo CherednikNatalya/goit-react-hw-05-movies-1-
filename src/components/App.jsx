4f8ac94a5bbb8aa039c434ae3d28914f5f4477a4
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-03-05 20:18:21 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import SharedLayout from './SharedLayout/SharedLayout'
+import Movies from '../pages/Movies/Movies'
+import Home from '../pages/Home/Home'
+import Cast from '../components/Cast/Cast'
+import MovieDetails from '../pages/MovieDetails/MovieDetails'
+import Reviews from '../components/Reviews/Reviews'
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+
+    <BrowserRouter basename="/goit-react-hw-05-movies-1-">
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />}/>
+        <Route path="/movies/:movieId" element={<MovieDetails/>}>
+          <Route path="cast" element={<Cast/>} />
+          <Route path="reviews" element={<Reviews/>} />
+        </Route>
+        
+      </Route>
+    </Routes>
+    </BrowserRouter>
   );
 };
+export default App

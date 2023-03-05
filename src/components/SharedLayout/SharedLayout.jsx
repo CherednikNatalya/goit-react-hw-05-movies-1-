3b6a7ec4c1f589b87b6034643821bfadb49afd78
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-03-05 20:26:18 +0200

    5

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,24 @@
-const SharedLayout =() => {
-    // return ()
-}
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import { Container, Header,  Link } from "../SharedLayout/SharedLayout.styled";
+
+ const SharedLayout = () => {
+  return (
+    <Container>
+      <Header>
+        
+        <nav>
+          <Link to="" end> Home</Link>
+          <Link to="about">About</Link>
+        
+        </nav>
+      </Header>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Outlet />
+      </Suspense>
+    </Container>
+  );
+};
+
 
 export default SharedLayout
